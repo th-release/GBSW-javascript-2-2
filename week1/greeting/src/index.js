a045c08ac2b1edd647fb2d6a4639ab5b7aeecc68
th-release <cth.release@gmail.com>
2023-03-27 14:51:22 +0900

    rename files

--- week1/greeting/src/index.js
+++
@@ -1,2 +0,0 @@
-var name = prompt('이름 입력해라.')
-document.write(`<b><big>${name}</big></b>님, 환영합니다.`)
--- week2/calcAge/src/index.js
+++
@@ -1,5 +0,0 @@
-function calc() {
-  const date = new Date();
-  const age = prompt();
-  document.getElementById('result').innerHTML=`<div id="result" class show>당신의 나이는 ${date.getFullYear() - parseInt(age) + 1}살 입니다.</div>`
-}
